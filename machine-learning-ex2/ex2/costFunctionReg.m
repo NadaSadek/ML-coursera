5698874efbfed652b583d7b5004494be79ee1386
Nada Sadek <nadasadek92@gmail.com>
2017-10-29 19:20:20 +0100

    ex2 regularized log reg

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,7 +17,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
+h = sigmoid(X*theta);
+J = ((1 / m)*(-y' * log(h) - (1-y)' * log(1 - h))) + (sum(theta(2:size(theta)).^2)* lambda / (2* m));
+grad = (1 / m)* X' * (h .- y);
+grad(2:size(grad)) = grad(2:size(theta)) .+ (theta(2:size(theta)) * lambda / m);
 
 
 
